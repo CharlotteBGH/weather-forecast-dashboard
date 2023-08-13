50b696f3049ad6a66225d701aa0feaacfb718628
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-13 17:53:45 +0100

    removed cards from HTML and added in Javascript, added for loop to return 6 days of weather data from OpenWeather API - date, temp, wind, humidity. Changed initial query url to include city to pull through user input in search bar, with click function

--- assets/script.js
+++ assets/script.js
@@ -3,39 +3,87 @@ $(document).ready(function () {
   //Add the day.js into the top div to pull through the date
 
   var chosenCity = $("#search-input");
-  var displayDateHeader = $("#datedisplay");
-  var searchForm = $("search-form");
-  var fiveDayForecast = $("forecast");
-  var city = chosenCity.value;
+  var todayWeather = $("#today");
+  var searchForm = $("#search-form");
+  var fiveDayForecast = $("#forecast");
   var searchedCities = [];
 
   // Display the current day's date
-  function displayDate() {
-    var todayDate = moment().format("ddd MM/DD/YYYY");
-    displayDateHeader.text(todayDate);
-  }
-  displayDate();
+  //function displayDate() {
+  //var todayDate = moment().format("DD/MM/YYYY");
+  //displayDateHeader.text(todayDate);
+  //}
+  //displayDate();
 
   // Event listener for search button
 
-  $("#search-button").on("click", function () {
+  $("#search-button").on("click", function (event) {
+    event.preventDefault();
+
+    var city = chosenCity.val();
+
     // Store API
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c6e073c97cdefe7c2943541b6a576268`;
-
-    //var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c6e073c97cdefe7c2943541b6a576268`;
-
+    var getCityDetails = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&appid=c6e073c97cdefe7c2943541b6a576268`;
     //event.preventDefault
+    //.val()
+    //.trim()
 
-    fetch(queryURL)
+    fetch(getCityDetails)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        var cityButton = $("<button>");
-        var cityTitle = $("<button>").text(data.city);
+        console.log(data);
+        var cityLat = data.coord.lat;
+        var cityLon = data.coord.lon;
+        var cityForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=metric&appid=c6e073c97cdefe7c2943541b6a576268`;
 
-        cityButton.append(city);
-        $(".input-group-append").append(cityButton);
+        fetch(cityForecast)
+          .then(function (resp) {
+            return resp.json();
+          })
+          .then(function (forecastData) {
+            console.log(forecastData);
+
+            // Today's date and weather dashboard
+            for (
+              let index = 0;
+              index < forecastData.list.length;
+              index += 1430
+            ) {
+              const element = forecastData.list[index];
+              console.log(element);
+              var dashboard = `<section id="today" class="mt-3" role="region" aria-live="polite">
+                  <h2>${element.dt_txt}</h2>
+                  <li>Temperature: ${element.main.temp}°C</li>
+                  <li>Wind: ${element.wind.speed} mph</li>
+                  <li>Humidity: ${element.main.humidity}%</li>
+                </section>`;
+              todayWeather.append(dashboard);
+            }
+
+            // 5 day forecast cards
+            for (let index = 8; index < forecastData.list.length; index += 7) {
+              const element = forecastData.list[index];
+              console.log(element);
+              var card = `<ul class="col-2 day">
+              <li>Date: ${element.dt_txt}</li>
+              <li>Icon</li>
+              <li>Temperature: ${element.main.temp}°C</li>
+              <li>Wind: ${element.wind.speed} mph</li>
+              <li>Humidity: ${element.main.humidity}%</li>
+            </ul>`;
+              fiveDayForecast.append(card);
+            }
+            element.dt_txt.format("DD/MM/YYYY");
+            // Reformat date format
+
+            // var cityButton = $("#search-button");
+            // var cityTitle = $("<btn search-button>").text(data.city);
+
+            // cityButton.append(city);
+            // $(".list-group").append(cityTitle);
+          });
       });
   });
 });
